c2b25f611d4168ea8da5325719cc8692bade9e89
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-10-25 19:57:53 +0300

    Create RestaurantReservationDbContext and add it to RestaurantReservation.Db.

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantReservation.Db
+{
+    public class RestaurantReservationDbContext : DbContext
+    {
+        public RestaurantReservationDbContext(DbContextOptions<RestaurantReservationDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantReservation.Db;
+
+var configuration = new ConfigurationBuilder()
+            .AddJsonFile("D:\\FTS_internship\\ORM\\Restaurant-Reservation-\\RestaurantReservation\\appsettings.json")
+            .Build();
+
+var connectionString = configuration.GetConnectionString("RestaurantReservationDbConnection");
+
+var serviceProvider = new ServiceCollection()
+    .AddDbContext<RestaurantReservationDbContext>(options =>
+    {
+        options.UseSqlServer(connectionString);
+    })
+    .BuildServiceProvider();
