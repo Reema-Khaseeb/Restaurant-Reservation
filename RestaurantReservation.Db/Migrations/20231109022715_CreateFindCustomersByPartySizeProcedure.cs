eeacfa1f51338a577caf05875961025ef0ccaaac
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-12-11 12:45:42 +0200

    Remove Migrations/ from `.gitignore `

---
+++ RestaurantReservation.Db/Migrations/20231109022715_CreateFindCustomersByPartySizeProcedure.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateFindCustomersByPartySizeProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1968));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1988));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1992));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1996));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1999));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3796));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3844));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3848));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3861));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3864));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(8307));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(8325));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(8329));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(8331));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(8333));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(1279));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(1324));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(1327));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(1330));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 7, 0, 380, DateTimeKind.Local).AddTicks(1333));
+        }
+    }
+}
---
+++ RestaurantReservation.Db/Migrations/20231211111219_RenameEmployeesWithRestaurantDetailsViews.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameEmployeesWithRestaurantDetailsViews : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(7764));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(7778));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(7780));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(7781));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(7783));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(3189));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(3222));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(3224));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(3226));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2023, 12, 11, 13, 12, 19, 569, DateTimeKind.Local).AddTicks(3228));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1968));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1988));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 3,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1992));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1996));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 591, DateTimeKind.Local).AddTicks(1999));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 1,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3796));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 2,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3844));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 3,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3848));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 4,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3861));
+
+            migrationBuilder.UpdateData(
+                table: "Reservations",
+                keyColumn: "ReservationId",
+                keyValue: 5,
+                column: "ReservationDate",
+                value: new DateTime(2023, 11, 9, 4, 27, 14, 590, DateTimeKind.Local).AddTicks(3864));
+        }
+    }
+}
