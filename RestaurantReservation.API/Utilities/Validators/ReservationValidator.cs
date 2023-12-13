2f7e3c077a62966a098b65dee8a161f309d46aed
Reema Khaseeb <reemafkhaseeb@gmail.com>
2023-12-13 14:10:43 +0200

    Create `ReservtionController` with mapping and validations

---
+++ RestaurantReservation.API/Utilities/Validators/ReservationValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace RestaurantReservation.API.Utilities.Validators
+{
+    public class ReservationValidator : AbstractValidator<ReservationDTO>
+    {
+        public ReservationValidator()
+        {
+            RuleFor(x => x.ReservationDate)
+                .NotNull()
+                .WithMessage("Reservation date is required");
+            RuleFor(x => x.PartySize)
+                .GreaterThan(0)
+                .WithMessage("Party Size must be greater than 0");
+        }
+    }
+}
